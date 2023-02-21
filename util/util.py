0c5e1be12499e91e177967c16ede1122cf71c300
Felix Bachmair <felix.bachmair@dectris.com>
2023-02-21 09:25:40 +0100

    better error message if type cannot be handeled

--- util/util.py
+++ util/util.py
@@ -53,8 +53,11 @@ def save_image(image_numpy, image_path, aspect_ratio=1.0):
         image_numpy (numpy array) -- input numpy array
         image_path (str)          -- the path of the image
     """
-
-    image_pil = Image.fromarray(image_numpy)
+    try:
+        image_pil = Image.fromarray(image_numpy)
+    except TypeError as e:
+        print('warning cannot handle {type(image_numpy)} ')
+        raise
     h, w, _ = image_numpy.shape
 
     if aspect_ratio > 1.0:
